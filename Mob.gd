d62c830617e50610fc7eb58ed6202b8918d8236f
aandrosov0 <aandrosov338@gmail.com>
2022-08-29 20:44:55 +0300

    commit

---
+++ Mob.gd
@@ -0,0 +1,9 @@
+extends RigidBody2D
+
+func _ready():
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
